0f4ecfaa7b96f8d1681f25c6b4ada37f519e5a14
Daniel Ladiginsky <daniel@shopanda.io>
2025-05-25 19:19:34 +0300

    initialize Cost Manager API with MongoDB connection and user/cost models

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Cost Manager API is running ');
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log(' Connected to MongoDB Atlas'))
+  .catch((err) => console.error(' MongoDB connection error:', err));
+
+const PORT = 4050;
+
+app.listen(PORT, () => {
+  console.log(` Server is running on http://localhost:${PORT}`);
+});
